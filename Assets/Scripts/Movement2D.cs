a82a3a3fe56745361a0f16ac32943e0a4d2be948
Xenonian <starcraft777777@gmail.com>
2020-10-12 16:06:19 +0900

    2020 10 12 tuto1  done
    2020 10 12 tuto1 done

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -5,6 +5,17 @@ public class Movement2D : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 0.0f;
     [SerializeField]
-    private Vector3 
+    private Vector3 moveDirection = Vector3.zero;
 
+    public float MoveSpeed => moveSpeed;
+
+    private void Update()
+    {
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    public void MoveTo(Vector3 direction)
+    {
+        moveDirection = direction;
+    }
 }
