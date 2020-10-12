9f8ce708577ef04566301e3b298fde663ada270f
Xenonian <starcraft777777@gmail.com>
2020-10-12 17:44:35 +0900

    2020 10-12 tuto2 6:11
    2020 10-12 tuto2 6:11

---
+++ Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerPrefab;
+
+    public void SpawnTower(Transform tileTransform)
+    {
+        Tile tile = tileTransform.GetComponent<Tile>();
+
+        if (tile.IsBuildTower == true)
+        {
+            return;
+        }
+
+        tile.IsBuildTower = true;
+
+        Instantiate(towerPrefab, tileTransform.position, Quaternion.identity);
+    }
+   
+}
