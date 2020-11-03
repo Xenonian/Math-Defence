815b77d301687ce8ce5f1a6ba91a11db09031506
Xenonian <starcraft777777@gmail.com>
2020-11-03 13:44:00 +0900

    2020 1103 tuto6
    2020 1103 tuto6

---
+++ Assets/Scripts/SystemTextViewer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public enum SystemType {Money = 0, Build}
+
+public class SystemTextViewer : MonoBehaviour
+{
+    private TextMeshProUGUI textSystem;
+    private TMPAlpha tmpAlpha;
+
+    private void Awake()
+    {
+        textSystem = GetComponent<TextMeshProUGUI>();
+        tmpAlpha = GetComponent<TMPAlpha>();
+    }
+
+    public void PrintText(SystemType type)
+    {
+        switch(type)
+        {
+            case SystemType.Money:
+                textSystem.text = "System : Not enough gold";
+                break;
+            case SystemType.Build:
+                textSystem.text = "System : Invalid space to build tower";
+                break;
+        }
+
+        tmpAlpha.FadeOut();
+    }
+}
